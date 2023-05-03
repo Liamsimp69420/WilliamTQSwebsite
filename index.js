25e2ffd1c857b4f4937c19dcfab96172c443e70d
William Quick <100945226+Liamsimp69420@users.noreply.github.com>
2023-05-03 21:34:56 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+
+<title> Willum </title> 
+<body>
+  <div class = "box">
+  </div> 
+  <div class = "square">
+  <p class = "grow"><b><a rel="noopener" target="_blank" href="#store">Store</a></b></p>
+  <p class = "grow"><b><a href="https://www.github.io">About us</a></b></p>
+  </div>
+  <br>
+  <br> 
+  <br> 
+  <br> 
+  <br> 
+  <br>
+  <br>
+  <h1><a id="store">Store</a></h1>
+  <div>
+  <p>Random Store item. </p><p>$-.--</p>
+  <img>
+  <br>
+  <p>Random Store item. </p><p>$-.--</p>
+  <br>
+  </div>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+</body>
+</head>
+<body>
+
+`;
